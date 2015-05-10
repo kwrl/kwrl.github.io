d37a9e22095f273a54b7c60390806637dc628018
kwrl <haakongk@stud.ntnu.no>
2015-05-10 20:06:30 +0200

    Working highlighting

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ $(document).ready(function() {
     $("#searchbox").on('keyup change', function() {
         var searchTerm = $(this).val();
         var elements = $("p, h1, h2, h3, h4");
+        $(".highlight").each(function() {
+            var p = $(this).parent();
+            var text = p.text();
+            $(this).remove();
+            p.text(text);
+        });
+        if(!searchTerm) {
+            return;
+        }
         elements.highlightText(searchTerm);
-        console.log(searchTerm);
-    })
+    });
 });
 
